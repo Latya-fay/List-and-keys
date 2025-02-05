aae7318985c3b8f5e3db6807890c59d21adc4e14
Latya-fay <ndanumulatya7@gmail.com>
2025-02-05 22:52:34 +0300

    Complete Labsssssss!

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,20 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
+
+// Import project data
 import user from "../data/user";
+import projects from "../data/projects";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
+      <Home name={user.name} city={user.city} color={user.color} />
       <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+
+      {/* Pass project data to ProjectList */}
+      <ProjectList projects={projects} />
     </div>
   );
 }
---
+++ src/components/projects.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    id: 1,
+    name: "Portfolio Website",
+    about: "A personal portfolio site to showcase my projects.",
+    technologies: ["React", "CSS", "JavaScript"],
+  },
+  {
+    id: 2,
+    name: "E-commerce Store",
+    about: "An online store with a shopping cart and checkout.",
+    technologies: ["Node.js", "MongoDB", "Express"],
+  },
+  {
+    id: 3,
+    name: "Chat App",
+    about: "A real-time chat application.",
+    technologies: ["Socket.io", "React", "Firebase"],
+  },
+];
+
+export default projects;
